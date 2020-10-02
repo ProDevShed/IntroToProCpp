d725e817b719da3cf4ecaacf3bc30595d1714c0e
ProDevShed <52107576+ProDevShed@users.noreply.github.com>
2020-10-02 11:46:31 +0100

    LivelyVet Instantiate variables

--- PT3/LivelyVet/LivelyVet/LivelyVet.cpp
+++ PT3/LivelyVet/LivelyVet/LivelyVet.cpp
@@ -3,5 +3,10 @@
 
 int main()
 {
-    
+    // Instantiate variables
+    std::string medication = "FleaAway";
+    std::string dosage = "100ml";
+    std::string useage = "Use as hair/body wash, once per day for six days.";
+    std::string misuse = "If treatment comes into contact with eyes or is ingested please see your closest coroner.";
+
 }
