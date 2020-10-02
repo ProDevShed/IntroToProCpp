fefcc7f391197ed410e0d3d386de8038c4f8c66f
ProDevShed <52107576+ProDevShed@users.noreply.github.com>
2020-10-02 12:18:45 +0100

    Guitarkulele - Print lines of text to console
    Also resolved some issues with text formatting.
    When inserting text copied from a work editor make sure to check all displayed text, text copied from an editor might be formatted and net appear like you think it should.

--- PT3/Guitarkulele/Guitarkulele/Guitarkulele.cpp
+++ PT3/Guitarkulele/Guitarkulele/Guitarkulele.cpp
@@ -4,12 +4,13 @@
 int main()
 {
     // Setup variables
-    std::string line1 = "e | --0------0—-0---------------0";
-    std::string line2 = "B | --1-/-3—-3-----3------------3";
-    std::string line3 = "G | --0------0--------0---------0";
-    std::string line4 = "D | --2-/-4—4------------4-------";
-    std::string line5 = "A | --3-/-5—5-----------------5--";
-    std::string line6 = "E | --0------0--------------0----";
+    std::string line1 = "e | --0------0--0---------------0\n";
+    std::string line2 = "B | --1-/-3--3-----3------------3\n";
+    std::string line3 = "G | --0------0--------0---------0\n";
+    std::string line4 = "D | --2-/-4-4------------4-------\n";
+    std::string line5 = "A | --3-/-5-5-----------------5--\n";
+    std::string line6 = "E | --0------0--------------0----\n\n";
 
-
+    // Print lines of text to console
+    std::cout << line1 << line2 << line3 << line4 << line5 << line6;
 }
