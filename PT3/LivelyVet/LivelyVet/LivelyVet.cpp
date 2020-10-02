ae7f1789434243d5ac9ba58bfa7b29cc1cdbc209
ProDevShed <52107576+ProDevShed@users.noreply.github.com>
2020-10-02 11:50:24 +0100

    LivelyVet Setup text lines

--- PT3/LivelyVet/LivelyVet/LivelyVet.cpp
+++ PT3/LivelyVet/LivelyVet/LivelyVet.cpp
@@ -9,4 +9,14 @@ int main()
     std::string useage = "Use as hair/body wash, once per day for six days.";
     std::string misuse = "If treatment comes into contact with eyes or is ingested please see your closest coroner.";
 
+    // Setup text lines
+    std::string alert = "**Dosage alert**";
+    std::string openingCurly = "{";
+    std::string line1 = "Prescribed medication, FleaAway, can be potentially to be deadly.";
+    std::string line2 = "Dosage:";
+    std::string line3 = "Instructions :";
+    std::string line4 = "Misuse guide :";    
+    std::string closingCurly = "}";
+
+
 }
