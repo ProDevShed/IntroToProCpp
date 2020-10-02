73c2bc50cab2cf4c2e8fa55bbfe5514ffa1bd18a
ProDevShed <52107576+ProDevShed@users.noreply.github.com>
2020-10-02 11:36:19 +0100

    Added lines of text

--- PT3/VentureCarHire/VentureCarHire/VentureCarHire.cpp
+++ PT3/VentureCarHire/VentureCarHire/VentureCarHire.cpp
@@ -3,8 +3,16 @@
 
 int main()
 {
-    // Instanciate variables
+    // Instantiate variables
     std::string carName = "Lincoln MKZ";
     std::string supervisor = "J Roberts";
     std::string value = "2500.99";
+
+    // Instantiate lines of text
+    std::string alert = "**ALERT**\n";
+    std::string lineOfStars = "**********************************************\n";
+    std::string line1 = "Lease of the " + carName +" is reserved for member only.\n";
+    std::string line2 = "For non - members this requires a completed non - member lease override form from supervisor " + supervisor + " and " + value + " deposit.\n";
+    std::string line3 = "Failure to follow these steps will result in disciplinary procedures and possible termination.\n";
+
 }
